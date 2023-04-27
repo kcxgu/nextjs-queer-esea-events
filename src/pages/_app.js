69df75538b259fd18d74c37c7c8d71737f794a66
Kaycee Gu <kayceegu@Kaycees-MacBook-Air.local>
2023-04-27 21:36:54 +0100

    added analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,12 +6,13 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Script src="https://www.clarity.ms/tag/" strategy="afterInteractive" />
       <Script id="clarity-analytic" strategy="afterInteractive">
         {`
           (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
           })(window, document, "clarity", "script", "gv4s2fdo6z");
       `}
       </Script>
